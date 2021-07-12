001d3b86748c518969e8a9ff5492527039eadf53
dannybanno <69281291+dannybanno@users.noreply.github.com>
2021-07-12 21:10:33 +0100

    Create login.py

---
+++ login.py
@@ -0,0 +1,24 @@
+from colorama import Fore, Back, Style
+import random
+import pyautogui
+
+
+print(Fore.RED + '''
+
+.__                .__        
+|  |   ____   ____ |__| ____  
+|  |  /  _ \ / ___\|  |/    \ 
+|  |_(  <_> ) /_/  >  |   |  |
+|____/\____/\___  /|__|___|  /
+           /_____/         \/
+
+''')
+
+loginUsername = input('Enter Username: ')
+
+loginPassword = input('Enter Password: ')
+
+
+if loginUsername == 'danny':
+    if loginPassword == 'danny2':
+        print('Welcome ' + loginUsername)
